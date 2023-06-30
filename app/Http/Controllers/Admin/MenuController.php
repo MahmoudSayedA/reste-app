169c51e2094949cd81d8c0f183aa39b49f67f2d3
Mahmoud Sayed <mahmoudsayed1332002@gmail.com>
2023-06-30 12:57:07 +0300

    update and delete menu - add update and delete methods - add edit view - add update request

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MenuStoreRequest;
+use App\Http\Requests\MenuUpdateRequest;
 use App\Models\Category;
 use App\Models\Menu;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class MenuController extends Controller
 {
@@ -69,16 +72,39 @@ class MenuController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(MenuUpdateRequest $request, Menu $menu)
     {
-        //
+        // get the old image
+        $image = $menu->image;
+        // check if new image added
+        if ($request->hasFile('image')) {
+            $imagePath = public_path('storage/' . $menu->image);
+            //delete the old
+            File::delete($imagePath);
+            //save the new
+            $image = $request->file('image')->store('uploads/menus/images', 'public');
+        }
+        $menu->update([
+            'name' => $request->name,
+            'description' => $request->description,
+            'image' => $image,
+            'price' => $request->price,
+        ]);
+
+        if ($request->has('categories')) {
+            $menu->categories()->sync($request->categories);
+        }
+        return to_route('admin.menus.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Menu $menu)
     {
-        //
+        $imagePath = public_path('storage/' . $menu->image);
+        File::delete($imagePath);
+        $menu->delete();
+        return to_route('admin.menus.index');
     }
 }
