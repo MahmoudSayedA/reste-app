b55534cd211f620650dcc3b5d16faf2d0803eea4
Mahmoud Sayed <mahmoudsayed1332002@gmail.com>
2023-07-09 17:53:56 +0300

    add frontend controller - add frontend category controller - add frontend menu controller - add  welcome controller - update guest template links - add categories views index, show - add categories views index

---
+++ app/Http/Controllers/Frontend/CategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return view('categories.index', compact('categories'));
+    }
+
+    public function show(Category $category)
+    {
+        $category->menus;
+        return view('categories.show', compact('category'));
+    }
+}
---
+++ app/Http/Controllers/Frontend/MenuController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Menu;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $menus = Menu::with('categories')->get();
+        // return $menus;
+        return view('menus.index', compact('menus'));
+    }
+}
