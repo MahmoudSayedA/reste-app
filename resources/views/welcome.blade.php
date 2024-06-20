373a700bfd68d7882b5bbfcc4e47f5dff67b7f46
Mahmoud Sayed <mahmoudsayed1332002@gmail.com>
2024-06-20 21:23:17 +0300

    update landing - add gradient

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <!-- Main Hero Content -->
-    <div class="overlay">
-        <div class="container max-w-lg px-4 py-32 mx-auto text-left bg-center bg-no-repeat bg-cover md:max-w-none md:text-center" style="background-image: url('https://cdn.pixabay.com/photo/2016/11/18/14/39/beans-1834984_960_720.jpg')">
+    <div class="container max-w-lg bg-center bg-no-repeat bg-cover md:max-w-none " style="background-image: url('https://cdn.pixabay.com/photo/2016/11/18/14/39/beans-1834984_960_720.jpg')">
+            <div class="overlay px-4 py-32 mx-auto text-left md:text-center">
             <h1 class="font-mono text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 md:text-center sm:leading-none lg:text-5xl">
                 <span class="inline md:block">Welcome To TailFood Restaurant</span>
             </h1>
